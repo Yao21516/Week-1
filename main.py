4dd496673204f400e60c79c1fb13b6842e2218ca
Yao21516 <love040102@example.com>
2022-09-30 18:20:38 +0800

    Week 1

---
+++ main.py
@@ -0,0 +1,17 @@
+if __name__ == "__main__":
+    #Extract numbers in the text file.
+    f = open("day1.txt" , "r")
+    data = f.read().splitlines()
+
+    #Change the data type to int.
+    data_length = len(data)
+    for i in range(data_length):
+        data[i] = int(data[i])
+    
+    #main process.
+    #counter every increase case.
+    counter = 0
+    for i in range(data_length-1):
+        if data[i+1]-data[i] > 0:
+            counter += 1
+    print(counter)
